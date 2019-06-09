649b7e454234ebcddb72b8d91de443cab6a38fca
Henning Thomsen <ht1703@gmail.com>
2019-06-09 16:54:26 +0200

    Added server functions

--- server.py
+++ server.py
@@ -2,8 +2,48 @@
 """
 Created on Sun Jun  9 15:57:13 2019
 
-Server part of the Chat application
+Chat application, server part.
+
+Implementation of a server which serves several clients using threading.
 
 @author: HTH
 """
 
+import socket
+import threading
+
+"""
+Server setup, define sockets and port numbers
+"""
+
+"""
+Server functions
+
+(1): Accept connections: Create thread for each client
+(2): Receive message from a single client
+(3): Broadcast messages to clients
+ 
+"""
+
+"""
+This function accepts incoming connections from clients
+"""
+def acceptConnection():
+    
+    pass
+
+
+"""
+This function listens for messages from clients
+"""
+def receiveMessage():
+    
+    pass
+
+
+"""
+This function broadcasts messages to all clients
+"""
+def broadcastMessage():
+    
+    pass
